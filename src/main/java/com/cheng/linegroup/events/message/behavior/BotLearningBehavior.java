30acd8f9ea9e0840fa1686aa92468210a40b9fb8
Cheng® <mark22013333@gmail.com>
2024-07-23 23:44:31 +0800

    chore: 調整學習關鍵字格式，若錯誤則回應提示格式

--- src/main/java/com/cheng/linegroup/events/message/behavior/BotLearningBehavior.java
+++ src/main/java/com/cheng/linegroup/events/message/behavior/BotLearningBehavior.java
@@ -3,6 +3,7 @@ package com.cheng.linegroup.events.message.behavior;
 import com.cheng.linegroup.dto.WebhookEvent;
 import com.cheng.linegroup.entity.ReplyKeyword;
 import com.cheng.linegroup.enums.Api;
+import com.cheng.linegroup.exception.BizException;
 import com.cheng.linegroup.service.LineService;
 import com.cheng.linegroup.service.ReplyKeywordService;
 import com.cheng.linegroup.service.dto.LineMessage;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -21,9 +23,13 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class BotLearningBehavior implements TextMessageBehavior {
 
+    private static final String[] KEYWORDS = {"欸唐董看到", "唐董", "唐懂", "唐墥", "唐"};
+
+    private static final String[] SPLIT_KEYWORDS = {"就回", "就說", ":", "："};
+
     @Override
     public boolean canHandle(String text) {
-        return text.startsWith("欸唐董看到");
+        return Arrays.stream(KEYWORDS).anyMatch(text::startsWith);
     }
 
     @Override
@@ -34,8 +40,13 @@ public class BotLearningBehavior implements TextMessageBehavior {
         String text = event.getMessage().getText();
 
         try {
-            String replace = text.replaceFirst("欸唐董看到", "").replace("就回", ",");
+            String s = removeKeywords(text).trim();
+
+            String replace = Arrays.stream(SPLIT_KEYWORDS)
+                    .reduce(s, (result, target) -> result.replace(target, ","));
+
             log.info("===>replace:{}", replace);
+
             String[] keywordSplitString = replace.split(",");
 
             boolean isDataExist = saveReplyKeyword(userId, groupId, keywordSplitString, replyKeywordService);
@@ -46,13 +57,35 @@ public class BotLearningBehavior implements TextMessageBehavior {
                     ).build();
             lineService.CallMessageAPI(lineMessage, Api.LINE_MESSAGE_REPLY);
 
+        } catch (BizException b) {
+            log.error("Error processing the message{}", b.getMessage());
+            LineMessage lineMessage = LineMessage.builder().replyToken(event.getReplyToken())
+                    .messages(Collections.singletonList(
+                            LineMessage.Message.builder().msg(b.getMessage()).build())
+                    ).build();
+            lineService.CallMessageAPI(lineMessage, Api.LINE_MESSAGE_REPLY);
         } catch (Exception e) {
             log.error("Error processing the message", e);
         }
 
     }
 
+    private String removeKeywords(String text) {
+        return Arrays.stream(KEYWORDS)
+                .filter(text::startsWith)
+                .findFirst()
+                .map(k -> text.replaceFirst(k.trim(), ""))
+                .orElse(text);
+    }
+
     private boolean saveReplyKeyword(String userId, String groupId, String[] keywordSplitString, ReplyKeywordService replyKeywordService) {
+        if (keywordSplitString.length < 2) {
+            throw BizException.error("""
+                    格式錯誤，請用以下範例讓唐董學習
+                    KEYWORDS = {"欸唐董看到", "唐董", "唐懂", "唐墥", "唐"}
+                    SPLIT_KEYWORDS = {"就回", "就說", ":", "："};
+                    例如：欸唐董看到A就回B、唐董A就說B、唐A:B、唐A：B""");
+        }
         String keyword = keywordSplitString[0];
         ReplyKeyword replyKeywordByKeyword = replyKeywordService.getReplyKeywordByKeywordAndUidAndGid(keyword, userId, groupId);
         if (replyKeywordByKeyword == null) {
